178f75098940efb06b33810900aae5c2fb28ca8e
csh01470 <csh01470@gmail.com>
2021-08-11 01:27:09 +0900

    CH.18 데이터셋 유형과 구조 판별하기

--- Script/Ⅱ. 데이터 전처리/20210810_03.R
+++ Script/Ⅱ. 데이터 전처리/20210810_03.R
@@ -1,8 +1,8 @@
 #2021.08.10. TUE
 #Data_Analytics_With_R
 
-## CH.18 데이터셋 유형과 구조 확인하기
-#18.1. is() 함수로 데이터셋 유형 확인하기
+## CH.18 데이터셋 유형과 구조 판별하기
+#18.1. is() 함수로 데이터셋 유형 판별하기
 student_dataset <- data.frame(name = c('PSH', 'CGE', 'CSH', 'CMJ', 'CJH'),
                               gender = factor(c('m', 'f', 'm', 'f', 'm')),
                               korean = c(35, 92.5, 70,85, 60),
@@ -26,7 +26,7 @@ is.logical(student_dataset$test_pass)
 #(5) is.factor()
 is.factor(student_dataset$gender)
 
-#18.2. is() 함수로 데이터셋 구조 확인하기
+#18.2. is() 함수로 데이터셋 구조 판별하기
 #(1) is.atomic()
 is.atomic('Data_Analytics_With_R')
 
