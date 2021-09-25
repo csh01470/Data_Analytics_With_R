6a3b963a0501e1fe4e3bb581fd884ab2087194f2
csh01470 <csh01470@gmail.com>
2021-09-25 20:00:16 +0900

    CH.23 데이터셋 구조 변경하기

--- Script/Ⅱ. 데이터 전처리/20210815_01.R
+++ Script/Ⅱ. 데이터 전처리/20210815_01.R
@@ -2,7 +2,7 @@
 #Data_Analytics_With_R
 
 ## CH.23 데이터셋 구조 변경하기 
-#23.1. 데이터셋의 열 이름 변경하기
+#23.1. 데이터셋의 열 이름 변경
 student_dataset <- data.frame(name = c('PSH', 'CGE', 'CSH', 'CMJ', 'CJH'),
                               gender = factor(c('m', 'f', 'm', 'f', 'm')),
                               korean = c(35, 92.5, 70, 85, 60),
@@ -18,14 +18,14 @@ colnames(student_dataset)[2] <- 'sex'
 #(3) 필터링을 이용해 부분 열 이름 변경하기
 colnames(student_dataset)[names(student_dataset)=='sex'] = 'gender'
 
-#23.2. 데이터셋의 열 순서 변경하기
+#23.2. 데이터셋의 열 순서 변경
 #(1) 열 번호로 순서 변경하기.
 student_dataset <- student_dataset[, c(2, 1, 3, 4, 5)]
 
 #(2) 열 이름으로 순서 변경하기. 
 student_dataset <- student_dataset[, c('name', 'gender', 'korean', 'math', 'test_pass')]
 
-#23.3. 데이터셋의 행 이름 부여하기
+#23.3. 데이터셋의 행 이름 부여
 #(1) 전체 행 이름 부여하기
 rownames(student_dataset) <- c('950110', '941106', '940815', '960321', '950228')
 
