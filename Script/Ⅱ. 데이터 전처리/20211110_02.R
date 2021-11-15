c4ba5a67d9a01fe63d534843dc2f5edfb85802c3
csh01470 <csh01470@gmail.com>
2021-11-15 16:33:01 +0900

    CH.31. Tidyverse와 대표적인 패키지들

---
+++ Script/Ⅱ. 데이터 전처리/20211110_02.R
@@ -0,0 +1,45 @@
+#2021.11.10. WED
+#Data_Analytics_With_R
+
+## CH.31. Tidyverse와 대표적인 패키지들
+#31.1. 기존의 base 문법의 단점과 Tidyverse 등장 배경
+#(1) base 문법의 단점
+#SKIP
+
+#(2) Tidyverse의 등장 배경
+#SKIP
+
+#31.2. Tidyverse의 정의와 특징
+#(1) tidy data의 정의
+#SKIP
+
+#(2) tidyverse의 정의
+#SKIP
+
+#(3) tidyverse의 장점
+#SKIP
+
+#(4) tidyverse의 단점
+#SKIP
+
+#31.3. Tidyverse의 대표적인 패키지들 
+#(1) 데이터 호출 패키지, readr
+#SKIP
+
+#(2) 데이터 전처리 패키지, tidyr & dplyr
+#SKIP
+
+#(3) 시계열 전처리 패키지, lubridate
+#SKIP
+
+#(4) 텍스트 전처리 패키지, stringr
+#SKIP
+
+#(5) 함수형 프로그래밍 패키지, purrr
+#SKIP
+
+#(6) 시각화 패키지, ggplot2
+#SKIP
+
+#31.4. Tidyverse 설치하기
+install.packages('tidyverse')
