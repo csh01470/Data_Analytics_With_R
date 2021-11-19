a1211ca043d435ffc64a3c8094b0a99483b66df3
csh01470 <csh01470@gmail.com>
2021-11-19 18:30:38 +0900

    CH.13 외부 데이터 불러오기

--- Script/Ⅰ. R 기초/20210809_01.R
+++ Script/Ⅰ. R 기초/20210809_01.R
@@ -2,22 +2,45 @@
 #Data_Analytics_With_R
 
 ## CH.13 외부 데이터 불러오기
-#13.1. read() 함수
-#(1) read.table() 
-asset_status <- read.table(file='data/asset_status.tsv', 
+#13.1. 데이터셋 불러오기, read.table()
+#(1) read.table() 함수의 구조와 파라미터
+#SKIP
+
+#(2) 예시
+asset_status <- read.table(file='./data/asset_status.tsv', 
                            sep='\t', 
                            header=TRUE)
 
-#(2) read.csv()
-grade_list_01 <- read.csv(file='data/grade_list.csv',
+#13.2. csv 형식 데이터셋 불러오기, read.csv()
+#(1) write.csv() 함수의 구조와 파라미터 
+#SKIP
+
+#(2) 예시
+grade_list_01 <- read.csv(file='./data/grade_list.csv',
                           header=TRUE)
 
-#(3) read_excel()
+#13.3. xlsx 형식 데이터셋 불러오기, read_xlsx()
+#(1) readxl 패키지 설치 및 활성화
+#install.packages('readxl')
 library(readxl)
-grade_list_02 <- read_excel(path='data/grade_list.xlsx',
+
+#(2) read_xlsx() 함수의 구조와 파라미터
+#SKIP
+
+#(3) 예시
+grade_list_02 <- read_xlsx(path='./data/grade_list.xlsx',
                             sheet=1,
                             col_names=TRUE)
 
-#(4) read.spss()
+#13.4. sav 형식 데이터셋 불러오기, read.spss()
+#(1) foreign 패키지 활성화
 library(foreign)
+
+#(2) read.spss() 함수의 구조와 파라미터
+#SKIP
+
+#(3) 예시
 walfare_raw <- read.spss('data/(2020년 15차 한국복지패널조사) 데이터(beta1.1)_spss/Koweps_hpda15_2020_beta1.1.sav',                            to.data.frame=TRUE)
+
+#13.5. 환경창을 통해 불러오기
+#SKIP
