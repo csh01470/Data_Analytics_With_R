1ccffefb7bcb2a582d9a9738b7fa97591246f228
csh01470 <csh01470@gmail.com>
2022-05-11 22:08:27 +0900

    2022-05-11 (WED)

--- Script/Ⅰ. R 기초/CH.16 제어문 파악하기.R
+++ Script/Ⅰ. R 기초/CH.16 제어문 파악하기.R
@@ -2,7 +2,7 @@
 # TITLE        :: CH16. 제어문 파악하기
 # AUTHOR       :: DATA_ANALYTICS_WITH_R, CHOE SANG HEON
 # CREATED DATE :: 2021-08-09 (MON)
-# UPDATED DATE :: 2022-05-09 (MON)
+# UPDATED DATE :: 2022-05-10 (TUE)
 
 ################################################################################
 #16.1. 조건문
@@ -37,7 +37,7 @@ for(i in 10:1){
 }
 
 #(2) while문
-tanker_HP     <- 100             #전사의 체력 설정
+tanker_HP     <- 100             #탱커의 체력 설정
 attack_damage <- sample(0:30, 1) #몬스터의 공격력 설정
 while(tanker_HP>0){
   Sys.sleep(1)
@@ -54,10 +54,10 @@ while(tanker_HP>0){
 
 #(3) repeat문
 repeat{
-  player1_index_bullet = sample(x=1:6, size=1)
-  player2_index_bullet = sample(x=1:6, size=1)
-  player1_index_trigger = sample(x=1:6, size=1)
-  player2_index_trigger = sample(x=1:6, size=1)
+  player1_index_bullet = sample(x=1:6, size=1)  #player1의 총알 인덱스 설정
+  player2_index_bullet = sample(x=1:6, size=1)  #player2의 총알 인덱스 설정
+  player1_index_trigger = sample(x=1:6, size=1) #player1의 초기 격발 인덱스 설정
+  player2_index_trigger = sample(x=1:6, size=1) #player2의 초기 격발 인덱스 설정
   if(player1_index_bullet == player1_index_trigger){
     Sys.sleep(time=1)
     cat("Tang! player1 is dead.","\n")
@@ -65,9 +65,9 @@ repeat{
   } else{
     Sys.sleep(time=1)
     cat("Tick! player1 is alive.", "\n")
-    player1_index_trigger = player1_index_trigger + 1L
+    player1_index_trigger = player1_index_trigger + 1L #player1의 격발 인덱스 업데이트
     if(player1_index_trigger==7L){
-      player1_index_trigger=1L
+      player1_index_trigger=1L #player1의 격발 인덱스가 6이 넘어갈 경우, 1로 초기화
     }
   }
   if(player2_index_bullet == player2_index_trigger){
@@ -77,11 +77,19 @@ repeat{
   } else{
     Sys.sleep(time=1)
     cat("Tick! player2 is alive.", "\n")
-    player2_index_trigger = player2_index_trigger + 1L
+    player2_index_trigger = player2_index_trigger + 1L #player2의 격발 인덱스 업데이트
     if(player2_index_trigger==7L){
-      player2_index_trigger=1L
+      player2_index_trigger=1L #player2의 격발 인덱스가 6이 넘어갈 경우, 1로 초기화
     }
   }
 }
+
+################################################################################
+#16.3. 분기문
+#(1) break문
+#SKIP
+
+#(2) next문
+#SKIP
 
 ################################################################################
